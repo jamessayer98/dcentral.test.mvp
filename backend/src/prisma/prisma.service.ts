b8739dacb285ecf673fb56f557e130150ba32524
Abdullah Adeel <easyautosol@gmail.com>
2022-05-17 02:33:00 +0500

    ✅ added backend and `database` setup

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(private config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
+
+  async cleanDB(sure = false, skipChecks = false) {
+    if (sure && (skipChecks || process.env.NODE_ENV === 'test')) {
+      await this.user.deleteMany({});
+    }
+  }
+}
